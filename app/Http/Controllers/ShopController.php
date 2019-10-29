c18042c215ff9a032a7dbf22b8e645d5ca751942
十年饮冰 <735284361@qq.com>
2019-10-29 19:15:20 +0800

    123

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -34,7 +34,7 @@ class ShopController extends Controller
         foreach ($attr as $k=>$v) {
             $map['sku->'.$k] = $v;
         }
-        //  测试
+        //  测试123
         return GoodsSku::where($map)->where('goods_id',$id)->first();
     }
 }
