24f5245ba6b6f3d169764d05c082cfc7b3d85929
十年饮冰 <735284361@qq.com>
2019-10-31 22:27:29 +0800

    123

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -19,7 +19,7 @@ class PayController extends Controller
         $result = $app->order->unify([
             'body' => '原卤大亨',
             'out_trade_no' => date('YMDHis').rand(10000,99999),
-            'total_fee' => 101,
+            'total_fee' => 1,
             'spbill_create_ip' => '', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
             'notify_url' => 'http://dh.raohouhai.com/pay/callback', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             'trade_type' => 'JSAPI', // 请对应换成你的支付方式对应的值类型
