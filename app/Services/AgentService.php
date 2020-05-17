395b593a4c4c125dc3a1954cd59edc00ed726243
十年饮冰 <735284361@qq.com>
2020-05-17 23:18:55 +0800

    123

--- app/Services/AgentService.php
+++ app/Services/AgentService.php
@@ -600,6 +600,7 @@ class AgentService
             ->first();
         Log::info($orderNo);
         Log::info('最近的数据查询:',DB::getQueryLog());
+        DB::disableQueryLog();
         if ($agentOrderMaps !== null) {
             // 更新订单代理结算状态
             $this->setSettled($agentOrderMaps);
